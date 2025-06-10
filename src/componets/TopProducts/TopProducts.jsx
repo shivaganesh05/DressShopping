2cb0120424586e08e03c5b72606ca52655a461e2
shivaganeshchenna <shivaganesh.c@fervidsmart.com>
2025-06-10 14:46:14 +0530

    add cart

--- src/componets/TopProducts/TopProducts.jsx
+++ src/componets/TopProducts/TopProducts.jsx
@@ -121,7 +121,7 @@ const TopProducts = () => {
                   className="bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-primary"
                   onClick={() => handleAddToCart(data)}
                 >
-                  Add to Cartcd
+                  Add to Cart
                 </button>
               </div>
             </div>
